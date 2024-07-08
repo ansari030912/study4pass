71bebe5b48221ad6e0ac3273b5e1a2d79a124636
Hussnain <howrex95@gmail.com>
2024-07-08 14:57:50 +0500

    sxa

---
+++ app/exam-provider-1/sitemap.jsx
@@ -0,0 +1,20 @@
+export default async function sitemap() {
+  const response = await fetch("https://certsgang.com/v1/sitemap/vendors", {
+    headers: {
+      "x-api-key": "b46279cb-13bb-4445-a6f9-6f252b61ae79",
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  
+
+  return data.map((item) => ({
+    url: `https://exam-hero.netlify.app/exam-provider/${item.vendor_perma}`,
+    lastModified: "2024-04-17",
+    priority: 0.6,
+  }));
+}
---
+++ app/exam-questions-5/sitemap.jsx
@@ -0,0 +1,20 @@
+export default async function sitemap() {
+  const response = await fetch("https://certsgang.com/v1/sitemap/exams/5", {
+    headers: {
+      "x-api-key": "b46279cb-13bb-4445-a6f9-6f252b61ae79",
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  
+
+  return data?.exams?.map((item) => ({
+    url: `https://exam-hero.netlify.app/exam-questions/${item?.vendor_perma}/${item?.exam_perma}`,
+    lastModified: "2024-04-17",
+    priority: 0.6,
+  }));
+}
